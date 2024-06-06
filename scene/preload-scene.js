7e2e7f80ca914ba9fb157fbaaf5f6a01c06cff5e
hankgalt1 <hank_readen@mail.ru>
2024-06-06 18:02:12 +0200

    Update preload-scene.js

--- scene/preload-scene.js
+++ scene/preload-scene.js
@@ -40,7 +40,7 @@ export class PreloadScene extends Phaser.Scene{
             color:'#793b24' 
         });
 
-        this.preload_text = this.add.text(this.scale.width/2-80,-100, `${window.Telegram.WebApp.user}`, {
+        this.preload_text = this.add.text(this.scale.width/2-80,-100, `${window.Telegram.WebApp}`, {
             font: '100 40px font1',
             color:'#000000' 
         }).setOrigin(0);
